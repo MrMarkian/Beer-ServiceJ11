0d04466c98b70d6a6c620c6d623892ab3c85bca8
Markian Rutkowskyj <i-keep-laughing@myself.com>
2020-08-08 18:01:06 +0100

    Added data validation Closes #8

--- src/main/java/com/springboot/beerservice/web/model/BeerDto.java
+++ src/main/java/com/springboot/beerservice/web/model/BeerDto.java
@@ -3,26 +3,41 @@ package com.springboot.beerservice.web.model;
 
 import lombok.*;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Positive;
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.UUID;
+
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class BeerDto {
-    private UUID id;
-    private Integer version;
 
+    @Null
+    private UUID id;
+    @Null
+    private Integer version;
+    @Null
     private OffsetDateTime createdDate;
+    @Null
     private OffsetDateTime lastModifiedDate;
 
+    @NotBlank
     private String beerName;
 
+    @NotNull
     private BeerStyleEnum beerStyle;
 
+    @Positive
     private Long UPC;
+
+    @Positive
+    @NotNull
     private BigDecimal price;
 
     private Integer quantityOnHand;
