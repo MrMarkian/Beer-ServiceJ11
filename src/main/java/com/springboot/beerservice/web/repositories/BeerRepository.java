46a360887f0cbd21feeddd7356c2c12a0e306015
Markian Rutkowskyj <i-keep-laughing@myself.com>
2020-08-08 13:53:29 +0100

    Added Beer Repository Closes #6

---
+++ src/main/java/com/springboot/beerservice/web/repositories/BeerRepository.java
@@ -0,0 +1,10 @@
+package com.springboot.beerservice.web.repositories;
+
+import com.springboot.beerservice.domain.Beer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.UUID;
+
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+
+}
